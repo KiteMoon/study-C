f94220dcb44763de765a19a9fd0c558b82e25b0b
aliceofalice <q1662053777@qq.com>
2020-08-12 17:37:19 +0800

    添加多行if语句

---
+++ study/多行if判断.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a = 0;
+	cin >> a;
+	if (a > 529)
+	{
+		cout << "恭喜你考上本科一批次" << endl;
+	}
+	else
+	{
+		cout << "恭喜你没考上本科一批次" << endl;
+	}
+
+}
